22e8061cab56b2148d43f104114b7cdcc85099e1
moravcikj24 <moravcikj24@Z00T1-01>
2024-09-17 11:09:38 +0200

    cisla vela 17.9.2024

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Ako sa voláš?");
+﻿/*Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 //Console.WriteLine("Ahoj, " + meno + "\n");
 
@@ -10,9 +10,55 @@ int vek = int.Parse(vstup);
 /*Console.WriteLine("kolko penazi mas na ucte????");
 string money = Console.ReadLine() ?? "0";
 float peniaze = float.Parse(money);
-Console.WriteLine("Máš " + money + " $ GG" + "\n");*/
+Console.WriteLine("Máš " + money + " $ GG" + "\n");
 
 Console.WriteLine("ake je tvoje oblubene jedlo?");
 string? jedlo = Console.ReadLine();
 //Console.WriteLine("Hmmmmmmmmm " + jedlo + " znie dobre" );
-Console.WriteLine($"Volas sa {meno}, mas {vek} rokov a tvoje oblubene jedlo je {jedlo} ." + "\n");
+Console.WriteLine($"Volas sa {meno}, mas {vek} rokov a tvoje oblubene jedlo je {jedlo} ." + "\n"); */
+
+
+/*int prvecislo = 20;
+int druhecislo = 5;
+Console.WriteLine(prvecislo + druhecislo);
+Console.WriteLine(prvecislo - druhecislo);
+Console.WriteLine(prvecislo /  druhecislo);
+Console.WriteLine(prvecislo * druhecislo);
+
+Console.WriteLine(prvecislo == druhecislo);
+Console.WriteLine(prvecislo != druhecislo);
+Console.WriteLine(prvecislo <  druhecislo);
+Console.WriteLine(prvecislo > druhecislo);
+Console.WriteLine(prvecislo >= druhecislo);
+Console.WriteLine(prvecislo <= druhecislo);*/
+
+
+
+
+//databazy
+var menopouzivatela = "Jakub";
+var heslopouzivatela = 125;
+
+//zadane uzivatelom
+var zadanemeno = "Jakub";
+var zadaneheslo = 125;
+
+//var porovnanieMena = menopouzivatela == zadanemeno;
+//var porovnavanieHesla = heslopouzivatela == zadaneheslo;
+//var prijatPristup = porovnanieMena && porovnavanieHesla;
+
+/*Console.WriteLine("Meno zhoda: " + porovnanieMena);
+Console.WriteLine("Heslo zhoda: " + porovnavanieHesla);
+Console.WriteLine("pustime pouzivatela: " + prijatPristup);*/
+
+//if (prijatPristup)
+if (menopouzivatela == zadanemeno && heslopouzivatela == zadaneheslo)
+{
+    Console.WriteLine("heslo sa zhoduje, vitaj! " + menopouzivatela);
+    //
+    //
+}
+else
+{
+    Console.WriteLine("Heslo sa NEZHODUJE, mas este 3 pokusy inak budes elminovany");
+}
