e64bde2d72a8d200d41d627cc734bf67460716ad
moravcikj24 <moravcikj24@Z00T1-01>
2024-10-01 11:05:45 +0200

    list+cykly cvicenie

---
+++ list+czk/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadaj meno");
+           
+
+            List<string> listnames = new List<string>();
+            listnames.Add(Console.ReadLine());
+            listnames.Add(Console.ReadLine());
+            listnames.Add(Console.ReadLine());
+            listnames.Add(Console.ReadLine());
+
+            foreach (string name in listnames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
