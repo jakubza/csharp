95cd276e32f0b86d4ebedc97088510e186b3eeb9
moravcikj24 <moravcikj24@Z00T1-01>
2024-10-01 10:07:44 +0200

    cykly a list

---
+++ cvicenie cykli/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+                //i = i + 10000;
+                
+                
+                if (i == 10)
+                {
+                    break;
+                }
+               
+            }
+            
+            
+            
+            /*for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+
+
+
+            string[] names = { "Sam", "Tom", "Paulie", "Salieri" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            Console.WriteLine("zadaj cislo");
+                int pocetOpakovani = int.Parse(Console.ReadLine());
+
+            int b = 0;
+            while (b < 5)
+            {
+                Console.WriteLine(i);
+                b++;
+            
+            if (b == pocetOpakovani)
+           
+                break;
+            }
+
+            
+        }
+
+
+    }
+}
---
+++ cvicenie_list/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int[] numbers = new int[5];
+
+              //15 15 20 25 45
+              numbers[0] = 10;
+              numbers[1] = 15;
+              numbers[2] = 20;
+              numbers[3] = 25;
+              numbers[4] = 45;
+
+             //Console.WriteLine(numbers[2]);
+             Console.WriteLine(numbers[2] + numbers[4]);
+
+             numbers[4] = 10;
+             Console.WriteLine(numbers[2] + numbers[4]);
+
+
+
+             List<int> listnumbers = new List<int>();
+
+                 listnumbers.Add(10);
+                 listnumbers.Add(15);
+                 listnumbers.Add(20);
+                 listnumbers.Add(25);
+                 listnumbers.Add(45);
+
+             listnumbers[1] = 10000;
+
+             Console.WriteLine(listnumbers[1] + listnumbers[2]); */
+
+
+
+            List<string> listnames = new List<string>();
+
+            listnames.Add("Jozo");
+            listnames.Add("Fero");
+            listnames.Add("Andrej");
+            listnames.Add("Martin");
+            listnames.Add("Jakub");
+            listnames.Add("Karol");
+
+            Console.WriteLine(listnames[0] + "\n" + listnames[1] + "\n" + listnames[2] + "\n" + listnames[3] + "\n" + listnames[4] + "\n" + listnames[5]);
+
+
+            listnames.Remove("Jakub");
+            Console.WriteLine(listnames[4]);
+
+           
+
+
+
+
+        }
+    }
+}
