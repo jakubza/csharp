4a7488f1ed6ea9b8baa4bd1b82512f68798ea427
moravcikj24 <moravcikj24@Z00T1-01>
2024-11-12 08:19:58 +0100

    LINQ - kniha

---
+++ LINQ - kniha/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LINQ___kniha
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Kniha> Knihy = new List<Kniha>()
+            {
+            new Kniha { Autor = "Anna Maria", RokVydania = 2011, JePreDeti = true },
+            new Kniha { Autor = "Anna Hruskova",RokVydania = 2009, JePreDeti = false },
+            new Kniha { Autor = "Bob", RokVydania = 2022, JePreDeti = true },
+            new Kniha { Autor = "Charlie", RokVydania = 2018, JePreDeti = true },
+            new Kniha { Autor = "Bob", RokVydania = 2020 , JePreDeti = false},
+            };
+
+
+            List<Kniha> knihaJePreDeti = Knihy.Where(kniha => kniha.JePreDeti == true).ToList();
+            foreach (Kniha kniha in knihaJePreDeti)
+            {
+                Console.WriteLine($" Autor: {kniha.Autor}, {kniha.RokVydania}. Je pre deti ? { kniha.JePreDeti}");
+            }
+        }
+
+        
+    }
+}
---
+++ LINQ - kniha/kniha.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ___kniha
+{
+    internal class Kniha
+    {
+        public string Autor { get; set; }
+        public  int RokVydania { get; set; }
+        public bool JePreDeti { get; set; }
+    }
+}
